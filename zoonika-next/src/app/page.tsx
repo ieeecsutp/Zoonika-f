ac87bbf9339171238d30617d025360e154fd9e1d
jhonyrd <jhonyrodri156@gmail.com>
2025-07-31 16:59:35 -0500

    "mis cambios en el frontend"

--- zoonika-next/src/app/page.tsx
+++ zoonika-next/src/app/page.tsx
@@ -2,14 +2,14 @@
 "use client";
 // @ts-nocheck
 
-import HeaderNav from "../components/HeaderNav.jsx";
-import Hero from "../components/hero.jsx";
+import HeaderNav from "../components/commons/HeaderNav.jsx";
+import Hero from "../components/Hero/heroMain.jsx";
 import AcercaDeNosotros from "../components/AcercaDeNosotros.jsx";
 import Services from "../components/Services.jsx";
 import Testimonials from "../components/Testimonials.jsx";
 import Galeria from "../components/Galeria.jsx";
 import Contacto from "../components/Contacto.jsx";
-import Footer from "../components/Footer.jsx";
+import Footer from "../components/commons/Footer.jsx";
 import React from "react";
 
 export default function HomePage() {
