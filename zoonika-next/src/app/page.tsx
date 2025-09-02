199bd66a5bc15ed7129f10946ba693ee22011151
jhonyrd <jhonyrodri156@gmail.com>
2025-09-01 20:04:00 -0500

    chore: corregir ñas importaciones con extensión .jsx a tsx

--- zoonika-next/src/app/page.tsx
+++ zoonika-next/src/app/page.tsx
@@ -2,14 +2,14 @@
 "use client";
 // @ts-nocheck
 
-import HeaderNav from "../components/commons/HeaderNav.jsx";
-import Hero from "../components/Hero/heroMain.jsx";
-import AcercaDeNosotros from "../components/AcercaDeNosotros.jsx";
-import Services from "../components/Services.jsx";
-import Testimonials from "../components/Testimonials.jsx";
-import Galeria from "../components/Galeria.jsx";
-import Contacto from "../components/Contacto.jsx";
-import Footer from "../components/commons/Footer.jsx";
+import HeaderNav from "../components/commons/HeaderNav";
+import Hero from "../components/Hero/heroMain";
+import AcercaDeNosotros from "../components/AcercaDeNosotros";
+import Services from "../components/Services";
+import Testimonials from "../components/Testimonials";
+import Galeria from "../components/Galeria";
+import Contacto from "../components/Contacto";
+import Footer from "../components/commons/Footer";
 import React from "react";
 
 export default function HomePage() {
